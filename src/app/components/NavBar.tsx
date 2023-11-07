818f118dd7edb46da3f44d16279a309da9167ad3
hlysllrs <hailey.sellars@gmail.com>
2023-11-06 21:59:37 -0500

    nav bar setup

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,5 +1,28 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import RCILogo from '../../../public/rci-logo.png'
+
+import { NAV_LINKS } from '../constants'
+
 const NavBar = () => {
-  return <div>NavBar</div>
+  return (
+    <nav className="flex justify-between m-4">
+      <Link href="/">
+        <Image src={RCILogo} alt="logo" height={100} width={200} />
+      </Link>
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  )
 }
 
 export default NavBar
---
+++ src/app/constants/index.ts
@@ -0,0 +1,11 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/about', key: 'about', label: 'About' },
+  { href: '/careers', key: 'careers ', label: 'Careers ' },
+  { href: '/projects', key: 'projects', label: 'Projects' },
+  { href: '/sales', key: 'sales', label: 'Sales' },
+  { href: '/services', key: 'services', label: 'Services' },
+  { href: '/safety', key: 'safety', label: 'Safety' },
+  { href: '/contact', key: 'contact', label: 'Contact' },
+]

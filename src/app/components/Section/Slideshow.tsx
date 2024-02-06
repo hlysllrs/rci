874b6bbe90baccd432ba2ccb5d15574789d34a89
hlysllrs <hailey.sellars@gmail.com>
2024-02-05 22:53:48 -0500

    carousel progress

--- src/app/components/Section/Slideshow.tsx
+++ src/app/components/Section/Slideshow.tsx
@@ -1,11 +1,18 @@
 'use client'
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useCallback } from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
+import { EmblaOptionsType } from 'embla-carousel'
 import Autoplay from 'embla-carousel-autoplay'
+import Image from 'next/image'
+import image1 from 'public/RCI_HOME_1.jpg'
 
-const Slideshow = () => {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false }, [Autoplay()])
+type PropType = {
+  options?: EmblaOptionsType
+}
+
+const Slideshow = (props: PropType) => {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()])
 
   useEffect(() => {
     if (emblaApi) {
@@ -13,14 +20,40 @@ const Slideshow = () => {
     }
   }, [emblaApi])
 
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev()
+  }, [emblaApi])
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext()
+  }, [emblaApi])
+
   return (
     <section>
-      <div className="embla" ref={emblaRef}>
-        <div className="embla__container">
-          <div className="embla__slide">Slide 1</div>
-          <div className="embla__slide">Slide 2</div>
-          <div className="embla__slide">Slide 3</div>
+      <div className="overflow-hidden" ref={emblaRef}>
+        <div className="flex">
+          <div className="embla__slide">
+            <Image src={image1} alt="image1" height={400} />
+          </div>
+          <div className="embla__slide">
+            <Image src={image1} alt="image1" height={400} />
+          </div>
+          <div className="embla__slide">
+            <Image src={image1} alt="image1" height={400} />
+          </div>
+          <div className="embla__slide">
+            <Image src={image1} alt="image1" height={400} />
+          </div>
+          <div className="embla__slide">
+            <Image src={image1} alt="image1" height={400} />
+          </div>
         </div>
+        <button className="embla__prev" onClick={scrollPrev}>
+          Prev
+        </button>
+        <button className="embla__next" onClick={scrollNext}>
+          Next
+        </button>
       </div>
     </section>
   )
